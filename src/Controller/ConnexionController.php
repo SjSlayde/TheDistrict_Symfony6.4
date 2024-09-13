754161b2274ec53f18ddc9d49078329652869dfd
houbron <benjamin.houbrongmail.com>
2024-09-13 10:55:03 +0200

    c'est le week end

---
+++ src/Controller/ConnexionController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\HttpFoundation\Request;
+use Doctrine\ORM\EntityManagerInterface;
+use App\Entity\Utilisateur;
+// use App\Form\Utili;
+
+class ConnexionController extends AbstractController
+{
+    #[Route('/connexion', name: 'app_connexion')]
+    public function index(): Response
+    {
+        return $this->render('connexion/index.html.twig', [
+            'controller_name' => 'ConnexionController',
+        ]);
+    }
+}
